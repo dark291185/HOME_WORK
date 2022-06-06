0c684ab01ffeddc90f3c2413c30fb5734c49e0f5
Ruslan <dark291185@gmail.com>
2022-06-06 22:10:52 +0300

    Completed!!!

---
+++ HomeWork4/4.py
@@ -0,0 +1,14 @@
+import utils as u
+
+print(u.currency_rates('USD'))
+print(u.currency_rates('EUR'))
+print(u.currency_rates('AMD'))
+print(u.currency_rates('HUF'))
+print(u.currency_rates('ABC'))
+"""
+73.505
+80.0249
+15.7304
+21.3914
+None
+""" 
